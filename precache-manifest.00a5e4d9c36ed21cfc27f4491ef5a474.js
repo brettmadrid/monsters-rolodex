fae91f6bd7d1a5e3fe1dd3bf7d1b4ce8e26cbadc
brettmadrid <brettmadrid9@gmail.com>
2020-02-21 17:15:36 -0800

    Updates

---
+++ precache-manifest.00a5e4d9c36ed21cfc27f4491ef5a474.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba55f348e7f3a38c4e5cc6691615766",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9cee7455029329a458d0",
+    "url": "/monsters-rolodex/static/css/main.425bcc60.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cee7455029329a458d0",
+    "url": "/monsters-rolodex/static/js/main.0e733473.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
